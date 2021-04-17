b41622750cfae9f18ad57a8adee4ccfdca10ad01
Victor Malukhin <victor.malukhin@gmail.com>
2021-04-17 17:18:44 +0300

    Swagger сonfigured

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+
+    @ApiProperty({example: 'user@mail.io', description: 'Уникальный e-mail'})
     readonly email: string;
+
+    @ApiProperty({example: 'P@ssword', description: 'Уникальный пароль'})
     readonly password: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
 
 interface UserCreationAttrs {
     email: string;
@@ -7,27 +8,35 @@ interface UserCreationAttrs {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs> {
+    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
     @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
+    @ApiProperty({example: 'userName', description: 'Уникальный логин пользователя'})
     @Column({ type: DataType.STRING, unique: true, allowNull: true  })
     login: string;
 
+    @ApiProperty({example: 'user@mail.io', description: 'Уникальный e-mail'})
     @Column({ type: DataType.STRING, unique: true, allowNull: false })
     email: string;
 
+    @ApiProperty({example: '+79000000000', description: 'Уникальный телефонный номер'})
     @Column({ type: DataType.STRING, unique: true, allowNull: true})
     phone: string;
 
+    @ApiProperty({example: 'P@ssword', description: 'Уникальный пароль'})
     @Column({ type: DataType.STRING, allowNull: false})
     password: string;
 
+    @ApiProperty({example: '12345678910', description: 'Уникальный ИНН пользователя (для онлайн касс)'})
     @Column({ type: DataType.INTEGER, unique: true, allowNull: true})
     individualTaxpayerNumber: number
 
+    @ApiProperty({example: 'false', description: 'Признак пользователя "Бан"'})
     @Column({ type: DataType.BOOLEAN, defaultValue: false  })
     banned: boolean;
 
+    @ApiProperty({example: 'some text', description: 'Причина бана'})
     @Column({ type: DataType.STRING, allowNull: true })
     banReason: string
 
